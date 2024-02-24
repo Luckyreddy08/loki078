8db3a483f38230dcfea1dc346d3cd8082837f758
Luckyreddy08 <160878409+Luckyreddy08@users.noreply.github.com>
2024-02-24 15:12:04 +0530

    addition,multiplication,subtraction on java

---
+++ addmulsub.java
@@ -0,0 +1,8 @@
+class addmulsub{
+       public static void main(String[] args){
+		int a=3,b=4;
+		System.out.println("result is " +(a+b));
+		System.out.println("result is " +(a-b));
+		System.out.println("result is " +(a*b));
+	}
+}  
